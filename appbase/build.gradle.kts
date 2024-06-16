324f3a4fbe6a031de44198a2124abab5814f6e0d
PhyoThihaWin <phyothiha2580@gmail.com>
2024-06-16 13:38:56 +0700

    github action added!

--- appbase/build.gradle.kts
+++ appbase/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.pthw.appbase"
+    namespace = BuildConfigConst.applicationId
     compileSdk = 34
 
     defaultConfig {
@@ -57,4 +57,28 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.timber)
+}
+
+object BuildConfigConst {
+    const val applicationId = "com.pocket.customer"
+    const val compileSdk = 34
+    const val minSdk = 23
+    const val targetSdk = 34
+
+    private const val versionMajor = 2
+    private const val versionMinor = 0
+    private const val versionPatch = 8
+    private const val versionBuild = 9
+    const val appVersionName = "$versionMajor.$versionMinor.$versionPatch"
+    const val appVersionCode = versionMajor * 1000000 + versionMinor * 10000 + versionPatch * 100 + versionBuild
+
+    const val devMinifyEnabled = false
+    const val uatMinifyEnabled = false
+    const val preprodMinifyEnabled = true
+    const val prodMinifyEnabled = true
+
+    const val devThreatCheck = false
+    const val uatThreatCheck = false
+    const val preprodThreatCheck = false
+    const val prodThreatCheck = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.jetbrainsKotlinJvm) apply false
     alias(libs.plugins.koltinxSerilization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.jetbrainsKotlinJvm) apply false
+    alias(libs.plugins.googleService) apply false
+    alias(libs.plugins.crashlytics) apply false
 }
 
 fun credentialData(): java.util.Properties {
